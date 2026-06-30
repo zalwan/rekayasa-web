cc32938feb0450014f7da938856c9a6ed2a2827a
rizalNCC <rizal@bawana.com>
2026-06-30 16:29:22 +0700

    feat: add sample product data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProdukKerajinan;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -24,5 +25,57 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('password'),
             ]
         );
+
+        $products = [
+            [
+                'id_produk' => 'PKT-001',
+                'nama_produk' => 'Vas Anyaman Rotan',
+                'bahan' => 'Rotan',
+                'harga' => 125000,
+                'pengrajin' => 'Rizal Suryawan',
+            ],
+            [
+                'id_produk' => 'PKT-002',
+                'nama_produk' => 'Tas Tenun Etnik',
+                'bahan' => 'Kain tenun',
+                'harga' => 225000,
+                'pengrajin' => 'Siti Aminah',
+            ],
+            [
+                'id_produk' => 'PKT-003',
+                'nama_produk' => 'Lampu Hias Bambu',
+                'bahan' => 'Bambu',
+                'harga' => 175000,
+                'pengrajin' => 'Asep Hidayat',
+            ],
+            [
+                'id_produk' => 'PKT-004',
+                'nama_produk' => 'Topeng Kayu Ukir',
+                'bahan' => 'Kayu mahoni',
+                'harga' => 95000,
+                'pengrajin' => 'Dedi Pratama',
+            ],
+            [
+                'id_produk' => 'PKT-005',
+                'nama_produk' => 'Keranjang Serbaguna',
+                'bahan' => 'Pandan',
+                'harga' => 85000,
+                'pengrajin' => 'Nanda Lestari',
+            ],
+            [
+                'id_produk' => 'PKT-006',
+                'nama_produk' => 'Dompet Kulit Handmade',
+                'bahan' => 'Kulit sapi',
+                'harga' => 150000,
+                'pengrajin' => 'Bima Santoso',
+            ],
+        ];
+
+        foreach ($products as $product) {
+            ProdukKerajinan::updateOrCreate(
+                ['id_produk' => $product['id_produk']],
+                $product
+            );
+        }
     }
 }
---
+++ tests/Feature/DatabaseSeederTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ProdukKerajinan;
+use Database\Seeders\DatabaseSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DatabaseSeederTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_database_seeder_adds_admin_and_sample_products(): void
+    {
+        $this->seed(DatabaseSeeder::class);
+
+        $this->assertDatabaseHas('users', ['username' => 'admin']);
+        $this->assertSame(6, ProdukKerajinan::count());
+        $this->assertDatabaseHas('produk_kerajinan', [
+            'id_produk' => 'PKT-001',
+            'nama_produk' => 'Vas Anyaman Rotan',
+            'bahan' => 'Rotan',
+            'pengrajin' => 'Rizal Suryawan',
+        ]);
+    }
+}
